5e4cde9ab4e2bac39ba680b565b1b5aa8ed52676
Ryan-Corsi <ryangabrielcorsi@gmail.com>
2022-05-27 16:16:12 -0300

    initial asasa

--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
 export const state = () => ({
     //BASE_URL: 'http://localhost:8000'
-    BASE_URL: 'https://manflix-ryan-corsi.scm.azurewebsites.net'
+    BASE_URL: 'https://manflix-ryan-corsi.azurewebsites.net'
 });
